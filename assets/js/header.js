e6b6c1bde8163e53e2f0586690da176832603d3d
Josue Aaron <sr.josue.aron@gmail.com>
2026-09-12 17:25:10 -0600

    Correción Header URL

--- assets/js/header.js
+++ assets/js/header.js
@@ -35,13 +35,27 @@ document.write(`
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="${ROOT}manuales.html" data-page="manuales.html">Manuales</a>
+            <a class="nav-link" href="https://www.facebook.com/share/p/1Bh8rgMhAi/" data-page="https://www.facebook.com/share/p/1Bh8rgMhAi/">
+              Referencias
+            </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="${ROOT}office2024.html" data-page="office2024.html">Office 2024 LTS</a>
+            <a class="nav-link" href="https://wa.me/525568942312?text=Hola!" data-page="office2024.html">
+              WhatsApp
+
+              <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+              <path d="M12 2C6.48 2 2 6.36 2 11.75c0 2.02.65 3.9 1.76 5.46L2.5 21.5l4.5-1.18a10.3 10.3 0 0 0 5 1.28c5.52 0 10-4.36 10-9.75S17.52 2 12 2Zm0 17.7c-1.6 0-3.1-.44-4.38-1.2l-.31-.18-2.68.7.72-2.55-.2-.32A7.9 7.9 0 0 1 4.1 11.75C4.1 7.47 7.66 4 12 4s7.9 3.47 7.9 7.75-3.56 7.95-7.9 7.95Zm4.3-5.9c-.24-.12-1.4-.68-1.62-.76-.22-.08-.38-.12-.54.12-.16.24-.62.76-.76.92-.14.16-.28.18-.52.06-.24-.12-1-.36-1.9-1.15-.7-.6-1.18-1.35-1.32-1.58-.14-.24-.02-.37.1-.49.11-.11.24-.28.36-.42.12-.14.16-.24.24-.4.08-.16.04-.3-.02-.42-.06-.12-.54-1.28-.74-1.76-.2-.46-.4-.4-.54-.4h-.46c-.16 0-.42.06-.64.3-.22.24-.84.8-.84 1.96 0 1.16.86 2.28.98 2.44.12.16 1.7 2.56 4.12 3.6.58.24 1.03.39 1.38.5.58.18 1.1.15 1.52.09.46-.07 1.4-.57 1.6-1.12.2-.55.2-1.02.14-1.12-.06-.1-.22-.16-.46-.28Z"/>
+              </svg>
+            </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="${ROOT}office365.html" data-page="office365.html">Office 365 Anual</a>
+            <a class="nav-link" href="https://m.me/michi.geeks.2025"" data-page="office365.html">
+            FB Messenger
+            
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+              <path d="M12 2C6.48 2 2 6.24 2 11.5c0 2.98 1.44 5.63 3.7 7.37V22l3.38-1.86c.9.25 1.87.38 2.92.38 5.52 0 10-4.24 10-9.5S17.52 2 12 2Zm1.02 12.79-2.55-2.72-4.98 2.72 5.48-5.82 2.6 2.72 4.93-2.72-5.48 5.82Z"/>
+            </svg>
+            </a>
           </li>
         </ul>
       </div>
